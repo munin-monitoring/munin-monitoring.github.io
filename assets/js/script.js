3b95ce18d197a73227acd6c1ade3066e4fedda6b
Quentin Stoeckel <chteuchteu@gmail.com>
2016-01-12 22:15:45 +0100

    Generate anchors for titles

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+/**
+ * Main JS file, included in all pages
+ */
+$(document).ready(function() {
+	// Anchors
+	var titles = $('.content').find('h1, h2, h3, h4, h5, h6');
+
+	// Create anchors for titles that hasn't one yet
+	// (markdown pages titles already has)
+	titles.not('[id]').not('.no-anchor').each(function() {
+		// Convert title to slug and add it as id
+		var slug = getSlug($(this).text());
+
+		// Check if it is unique
+		var i = 1;
+		while (titles.filter('#'  + slug).length) {
+			slug = getSlug($(this).text()) + '-' + i;
+			i++;
+		}
+
+		$(this).attr('id', slug);
+	});
+
+	// Add anchor next to them
+	titles.each(function() {
+		$('<a />')
+			.addClass('anchor')
+			.attr('href', '#' + $(this).attr('id'))
+			.attr('title', 'Link to this section')
+			.html('&para;')
+			.appendTo($(this));
+	});
+});
+
+function getSlug(text) {
+	return text.toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'-');
+}
